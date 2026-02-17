f4fcd6a485b2e78e9cc557cde35fe9181ebdb0f6
mkmani1998 <96861205+mkmani1998@users.noreply.github.com>
2026-02-18 02:02:03 +0530

    Change font-family from 'Syne' to 'Bebas Neue'

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ html, body, [class*="css"] {
 }
 
 h1, h2, h3 {
-    font-family: 'Syne', sans-serif !important;
+    font-family: 'Bebas Neue', sans-serif !important;
 }
 
 .block-container { padding-top: 2rem; padding-bottom: 2rem; }
